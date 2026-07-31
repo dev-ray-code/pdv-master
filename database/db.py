e3a0b9cac886c25235e75ea87b1319593a2621e5
dev-ray-code <pedroraymacedosantos1233@gmail.com>
2026-07-31 15:54:29 -0300

    conectar servidor ao PostgreSQL

--- database/db.py
+++ database/db.py
@@ -1,14 +1,25 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from database.models import Base
 
-DATABASE_URL = "sqlite:///pdv_store.db"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///pdv_store.db"
 )
+
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -16,12 +27,14 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
 
 def criar_banco():
     Base.metadata.create_all(bind=engine)
